6631bd8176d68bf9ca29a405b0caf97c0cbcebc7
Fabrice <mwanafunzifabrice@gmail.com>
2023-08-04 19:17:51 +0200

    changes

---
+++ dist/js/docs/basicInfo.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    openapi: "3.0.3",
+    info: {
+        title: "Komparas API",
+        description: "komparas from creativa poeta",
+        version: "1.0.0",
+        contact: {
+            name: "Fabrice MWANAFUNZI",
+            email: "fabrice.mwanafunzi@karisimbi.rw",
+            url: "web.com",
+        },
+    },
+};
---
+++ dist/js/docs/component.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    components: {
+        schemas: {
+            id: {
+                type: "string",
+                description: "An id of an object",
+                example: "tyVgf",
+            },
+            Hello: {
+                type: "object",
+                properties: {
+                    id: {
+                        $ref: "#/components/schemas/id",
+                    },
+                    hello: {
+                        type: "string",
+                        description: "Hello message",
+                        example: "hello world",
+                    },
+                },
+            },
+            Error: {
+                type: "object",
+                properties: {
+                    message: {
+                        type: "string",
+                        description: "Error message",
+                        example: "Not found",
+                    },
+                    internal_code: {
+                        type: "string",
+                        description: "Error internal code",
+                        example: "Invalid parameters",
+                    },
+                },
+            },
+        },
+    },
+};
