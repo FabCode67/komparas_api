c442bf40541b5c3617efb06e5227f1b3d8a48813
FabCode67 <mwanafunzifabrice@gmail.com>
2023-10-22 23:31:58 +0200

    fix

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -3,7 +3,7 @@ import { IContact } from '../types/contact';
 
 const ContactSchema = new Schema<IContact>({
     name: { type: String, required: true},
-    email: { type: String, default: null, required: true, unique: true },
+    email: { type: String, required: true},
     message: { type: String, required: true },
 }, { timestamps: true });
 
