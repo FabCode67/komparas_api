bf7d4e644db6c8235ab7252c8c0dfb4f462f0bd9
FabCode67 <mwanafunzifabrice@gmail.com>
2023-11-22 18:48:09 +0200

    delete category

--- src/controllers/categories/category.ts
+++ src/controllers/categories/category.ts
@@ -58,3 +58,44 @@ export const addCategory = async (req: Request, res: Response): Promise<void> =>
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+
+
+export const deleteCategory = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { category_id } = req.params;
+
+        if (!category_id) {
+            res.status(400).json({ message: 'Invalid category ID' });
+            return;
+        }
+
+        const categoryToDelete = await Category.findById(category_id);
+
+        if (!categoryToDelete) {
+            res.status(404).json({ message: 'Category not found' });
+            return;
+        }
+
+        if (categoryToDelete.children && categoryToDelete.children.length > 0) {
+            res.status(400).json({ message: 'Cannot delete a category with children. Delete the children first.' });
+            return;
+        }
+
+        if (categoryToDelete.parent_id) {
+            const parentCategory = await Category.findById(categoryToDelete.parent_id);
+
+            if (parentCategory) {
+                parentCategory.children = parentCategory?.children?.filter(childId => childId.toString() !== category_id) ?? [];
+                await parentCategory.save();
+            }
+        }
+
+        await categoryToDelete.deleteOne();
+
+        res.status(200).json({ message: 'Category deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- src/routes/categories/categories.routes.ts
+++ src/routes/categories/categories.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { addCategory, getAllCategories, getParentCategories } from '../../controllers/categories/category';
+import { addCategory, getAllCategories, getParentCategories, deleteCategory } from '../../controllers/categories/category';
 const router: Router = Router();
 
 router.post("/category/add", addCategory);
 router.get("/categories/all", getAllCategories);
 router.get("/categories/parents", getParentCategories);
+router.delete("/categories/:category_id", deleteCategory);
 
 export default router;
