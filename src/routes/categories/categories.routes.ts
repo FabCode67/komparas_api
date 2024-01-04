67ec2360c25c80a46eb45961fd01675e7fda1b10
FabCode67 <mwanafunzifabrice@gmail.com>
2024-01-05 00:27:41 +0200

    remove auth

--- src/routes/categories/categories.routes.ts
+++ src/routes/categories/categories.routes.ts
@@ -4,7 +4,7 @@ import { isAdminAuthenticat } from '../../middleware/auth/authorization';
 import { addCategory, getAllCategories, getParentCategories, deleteCategory, getCategoryByNameOrID } from '../../controllers/categories/category';
 const router: Router = Router();
 
-router.post("/category/add", isAdminAuthenticat, addCategory);
+router.post("/category/add", addCategory);
 router.get("/categories/all", getAllCategories);
 router.get("/categories/parents", getParentCategories);
 router.delete("/categories/:category_id", deleteCategory);
