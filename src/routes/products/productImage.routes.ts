fdc8f2d41178be9eedb0062efb7268ea71f37852
FabCode67 <mwanafunzifabrice@gmail.com>
2024-01-21 23:39:51 +0200

    removing errors

--- src/routes/products/productImage.routes.ts
+++ src/routes/products/productImage.routes.ts
@@ -2,13 +2,8 @@ import { Router } from "express";
 import { addProductImage } from "../../controllers/products/productImages";
 import { authenticat, isAdminAuthenticat } from "../../middleware/auth/authorization";
 import multer from "multer";
-
 const storage = multer.memoryStorage();
-
 const upload = multer({ storage });
-
 const router: Router = Router();
-
 router.post('/product_images/:product_id', upload.single('product_image'), addProductImage);
-
 export default router;
